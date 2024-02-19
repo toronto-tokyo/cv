3266bae548181feb91457c04e9c5b8af97087438
Minchuk Ilya <cacatubutyt@gmail.com>
2024-02-19 15:14:00 +0100

    feat: remove header

--- src/Layouts/RootLayout/RootLayout.tsx
+++ src/Layouts/RootLayout/RootLayout.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import LayoutWrapper from '../../components/LayoutWrapper';
-import Header from '../../components/Header';
 import Main from '../../components/Main';
 import Footer from '../../components/Footer';
 import MainPage from '../../pages/MainPage';
@@ -8,7 +7,6 @@ import MainPage from '../../pages/MainPage';
 const RootLayout: React.FC = () => {
   return (
     <LayoutWrapper>
-      <Header />
       <Main>
         <MainPage />
       </Main>
